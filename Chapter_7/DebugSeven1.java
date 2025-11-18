e9f75e06856fb2b23ff3a6e31f843b2d8c740a68
RonnLawrenceL.Jagna-UCM <ronn00045@gmail.com>
2025-11-18 13:42:22 +0800

    Add files via upload

---
+++ Chapter_7/DebugSeven1.java
@@ -0,0 +1,35 @@
+// Makes String comparisons
+import java.util.*;
+public class DebugSeven1
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        String name1, name2, name3;
+
+        System.out.println("Enter three names.");
+        System.out.println("They can be the same or different.");
+
+        System.out.print("First name >> ");
+        name1 = kb.nextLine();      // FIX: added ()
+
+        System.out.print("Second name >> ");
+        name2 = kb.nextLine();      // FIX: added ()
+
+        System.out.print("Third name >> ");
+        name3 = kb.nextLine();      // FIX: added ()
+
+        compareNames(name1, name2); // FIX: method name corrected
+        compareNames(name1, name3);
+        compareNames(name2, name3);
+    }
+
+    public static void compareNames(String n1, String n2) // FIX: method name + parameter type
+    {
+        System.out.print(n1 + " and " + n2);
+        if(n1.equals(n2))           // FIX: added missing )
+            System.out.println(" are the same");
+        else
+            System.out.println(" are different");
+    }
+}
